8887f624b1d8c29baa0ea5ef45099894ebd036c7
zcGreenBrid <1803331363@qq.com>
2019-11-22 14:32:33 +0800

    还原版本

--- FFMS-parent/FFMS-common/src/main/java/com/ffms/service/Test1.java
+++
@@ -1,7 +0,0 @@
-package com.ffms.service;
-
-public class Test1 {
-    public static void main(String[] args) {
-        System.out.print("xx");
-    }
-}
--- FFMS-parent/FFMS-common/src/main/java/com/ffms/service/test.java
+++
@@ -1,7 +0,0 @@
-package com.ffms.service;
-
-public class test {
-    public static void main(String[] args) {
-        
-    }
-}
